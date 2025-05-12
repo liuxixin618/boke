e8834c6ce3a981bee84141ac077fa37a9982cc6b
liuxixin <2658030236@qq.com>
2025-05-12 22:54:30 +0800

    U 增加500友好界面

--- app/main/routes.py
+++ app/main/routes.py
@@ -446,4 +446,8 @@ def inject_version():
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('main/404.html'), 404 
+    return render_template('main/404.html'), 404
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('main/500.html'), 500 
